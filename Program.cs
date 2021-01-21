1eafb526a9e32a8ef018d7ecad71485c7dd869ab
eliandersondev <elianderson.soares@gmail.com>
2021-01-21 11:54:01 -0300

    primeiro

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
